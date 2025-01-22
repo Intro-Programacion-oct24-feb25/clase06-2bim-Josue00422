c6132beb927055c906dac147c96d66c685627abd
unknown <jjnaula3@utpl.ediu.ec>
2025-01-22 12:53:51 -0500

    cambios2

---
+++ EjemplosExcepciones/src/manejoexepciones/Ejemplo092.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manejoexepciones;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+import java.util.Scanner;
+
+public class Ejemplo092 {
+
+    public static void main(String[] args) {
+         Scanner entrada = new Scanner(System.in);
+         String cadena ="";
+         boolean bandera = true;
+         while(bandera){
+             try {
+                 System.out.println("ingrese el nombre del pais");
+                 cadena = entrada.nextLine();
+                 String primer_caracter = cadena.substring(0,1);
+                 if(pirmer_caracter.equals(1,0))
+                 
+             }
+         }
+    }
+        
+    }
+       
+        
+
+      
