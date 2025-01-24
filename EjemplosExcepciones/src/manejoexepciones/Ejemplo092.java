c1c0b95570665611c50283cef2192b10216ad18f
jhandry naula <jjnaula3@utpl.edu.ec>
2025-01-24 10:35:47 -0500

    cambiosssss se logro xd

--- EjemplosExcepciones/src/manejoexepciones/Ejemplo092.java
+++ EjemplosExcepciones/src/manejoexepciones/Ejemplo092.java
@@ -17,22 +17,27 @@ import java.util.Scanner;
 public class Ejemplo092 {
 
     public static void main(String[] args) {
-         Scanner entrada = new Scanner(System.in);
-         String cadena ="";
-         boolean bandera = true;
-         while(bandera){
-             try {
-                 System.out.println("ingrese el nombre del pais");
-                 cadena = entrada.nextLine();
-                 String primer_caracter = cadena.substring(0,1);
-                 if(pirmer_caracter.equals(1,0))
-                 
-             }
-         }
-    }
-        
-    }
-       
-        
+        Scanner entrada = new Scanner(System.in);
+        String cadena = "";
+        boolean bandera = true;
+        while (bandera) {
+            try {
+                System.out.println("ingrese el nombre del pais");
+                cadena = entrada.nextLine();
+                String primer_caracter = cadena.substring(0, 1);
+                if (primer_caracter.equals("a") || primer_caracter.equals("z")) {
+                    throw new Exception("debe ingresar el nombre del pais con "
+                            + "una consonante inicial ");
 
-      
+                }
+                 bandera = false;
+               
+            } catch (Exception e) {
+                System.out.printf("ocurrio un error %s\n", e);
+
+            }
+        }
+        System.out.printf("%S\n", cadena);
+    }
+
+}
